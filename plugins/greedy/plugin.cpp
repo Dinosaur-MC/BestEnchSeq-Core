1b6d929a28c11e983fd7be9b8dc1ce88a6f298b1
Dinosaur_MC <Dinosaur_MC@outlook.com>
2026-07-28 04:08:16 +0800

    refactor(benchmarks): 调整基准测试用例的锻造成本参数
    - 将剑类物品的锻造成本从原来的值降低到更合理的数值
    - 工具类物品的锻造成本也相应调整以匹配新的基准
    - 弓箭和护甲类装备的成本参数同步更新
    - 针对地狱合金装备的复杂配方重新校准了成本估算
    
    BREAKING CHANGE: 基准测试结果将因成本参数调整而发生变化

--- plugins/greedy/plugin.cpp
+++
@@ -1,3 +0,0 @@
-#include "domain/algorithm/plugin/PluginEntry.h"
-#include "GreedyAlgorithm.h"
-BESQ_PLUGIN_ENTRY(algorithm::GreedyAlgorithm)
--- plugins/hierarchical/plugin.cpp
+++
@@ -1,3 +0,0 @@
-#include "domain/algorithm/plugin/PluginEntry.h"
-#include "HierarchicalMergeAlgorithm.h"
-BESQ_PLUGIN_ENTRY(algorithm::HierarchicalMergeAlgorithm)
